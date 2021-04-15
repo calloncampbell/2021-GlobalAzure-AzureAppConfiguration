5d1b5b70aa020562968ecc060aaf4ed6bd7f5de5
Callon Campbell <calloncampbell@msn.com>
2021-04-15 17:19:17 -0400

    Integrated Azure App Configuration

--- src/Demo1-AzureFunction/Startup.cs
+++ src/Demo1-AzureFunction/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Demo1_AzureFunction.Startup))]
@@ -13,6 +14,7 @@ namespace Demo1_AzureFunction
     {
         private static IConfigurationRoot Configuration { get; set; }
         public IConfigurationBuilder ConfigurationBuilder { get; set; }
+        private static IConfigurationRefresher ConfigurationRefresher { set; get; }
 
         public Startup()
         {
@@ -25,6 +27,29 @@ namespace Demo1_AzureFunction
 
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            // Load configuration from Azure App Configuration
+            ConfigurationBuilder.AddAzureAppConfiguration(options =>
+            {
+                var connectionString = Environment.GetEnvironmentVariable("AzureAppConfigConnectionString");
+                var cacheExpiryInSeconds = double.Parse(Environment.GetEnvironmentVariable("AzureAppConfigurationCacheExpirationTimeInSeconds") ?? "30");
+                var environmentLabel = !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("AzureAppConfigurationEnvironmentLabel"))
+                    ? Environment.GetEnvironmentVariable("AzureAppConfigurationEnvironmentLabel")
+                    : LabelFilter.Null;
+
+                // Use ".Connect(...)" for connection string, or use ".ConnectWithManagedIdentity(...) for managed identity"
+                options.Connect(connectionString)
+                       // Load all keys that start with `DemoFunction:`
+                       .Select("DemoFunction:*")
+                       .Select("DemoFunction:*", environmentLabel)
+                       // Configure to reload configuration if the registered 'Sentinel' key is modified
+                       .ConfigureRefresh(refreshOptions =>
+                            refreshOptions.Register(key: "DemoFunction:Sentinel", label: environmentLabel, refreshAll: true)
+                                          .SetCacheExpiration(TimeSpan.FromSeconds(cacheExpiryInSeconds))
+                       );
+                ConfigurationRefresher = options.GetRefresher();
+            });
+            Configuration = ConfigurationBuilder.Build();
+
             builder.Services.AddLogging();
             builder.Services.AddSingleton(Configuration);
         }
